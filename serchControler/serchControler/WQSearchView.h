7faa0c2f4025f1d9588dea4b87861588d6bed7a7
luwenqing <luwenqing@szboanda.net>
2019-11-28 10:41:03 +0800

    添加搜索view

---
+++ serchControler/serchControler/WQSearchView.h
@@ -0,0 +1,17 @@
+//
+//  WQSearchView.h
+//  serchControler
+//
+//  Created by lwq on 2019/11/28.
+//  Copyright © 2019 luwenqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WQSearchView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ serchControler/serchControler/WQSearchView.m
@@ -0,0 +1,21 @@
+//
+//  WQSearchView.m
+//  serchControler
+//
+//  Created by lwq on 2019/11/28.
+//  Copyright © 2019 luwenqing. All rights reserved.
+//
+
+#import "WQSearchView.h"
+
+@implementation WQSearchView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
